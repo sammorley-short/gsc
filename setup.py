71c6ce05f12524ef19f1e6341460b12cdbe41260
Sam Morley-Short <sam.morleyshort@gmail.com>
2020-10-11 16:27:12 -0700

    [setup] Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 
-setup(name='gsc',
-      version='2.0',
-      description='"gsc" or "graph state compass" is a Python package containing a number of tools used for mapping and depicting the local Clifford equivalence classes of quantum graph states.',
-      url='https://github.com/sammorley-short/gsc',
-      author='Sam Morley-Short',
-      license='GNU General Public License v3.0',
-      packages=['gsc'],
-      install_requires=[
-          'abp',
-          'numpy',
-          'sympy',
-          'matplotlib==2.2.3',
-          'networkx'],
-      zip_safe=False)
+setup(
+    name='gsc',
+    version='2.0',
+    description='"gsc" or "graph state compass" is a Python package containing tools for mapping and depicting the '
+    'local Clifford equivalence classes of quantum graph states.',
+    url='https://github.com/sammorley-short/gsc',
+    author='Sam Morley-Short',
+    license='GNU General Public License v3.0',
+    packages=['gsc'],
+    install_requires=[
+        'abp',
+        'numpy',
+        'sympy',
+        'matplotlib==2.2.3',
+        'networkx'
+    ],
+    zip_safe=False
+)
