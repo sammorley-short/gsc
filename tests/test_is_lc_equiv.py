a32b0195841f945853f2ee218ca83eed8d474ba3
Sam Morley-Short <sam.morleyshort@gmail.com>
2021-06-06 23:04:37 -0700

    [tests] Refactored is_lc_equiv testsuite

--- tests/test_is_lc_equiv.py
+++ tests/test_is_lc_equiv.py
@@ -1,4 +1,5 @@
 # Python modules
+import pytest
 import networkx as nx
 from random import randint
 # Local modules
@@ -7,25 +8,26 @@ from gsc.explore_lc_orbit import apply_qubit_LCs
 from gsc.is_lc_equiv import get_adjacency_matrix, are_lc_equiv
 
 
-def test_is_lc_equiv():
+@pytest.mark.parametrize('n_v', [4, 5, 6])
+@pytest.mark.parametrize('n_lcs', [5, 10])
+def test_is_lc_equiv(n_v, n_lcs):
     """
-        Tests is_lc_equiv by creating graphs, applying LCs and checking for
-        equivalence.
+    Tests is_lc_equiv by creating graphs, applying LCs and checking for
+    equivalence.
     """
-    n, lcs = 6, 10
-    for _ in range(100):
-        lc_nodes = [randint(0, n - 1) for _ in range(lcs)]
-        graph_init = random_connected_graph(n)
+    for _ in range(25):
+        lc_nodes = [randint(0, n_v - 1) for _ in range(n_lcs)]
+        graph_init = random_connected_graph(n_v)
         graph_fin = apply_qubit_LCs(graph_init, lc_nodes)
-        lc_equiv, lc_ops = are_lc_equiv(graph_init, graph_fin)
+        lc_equiv, _ = are_lc_equiv(graph_init, graph_fin)
         assert lc_equiv
 
 
-def test_get_adjacency_matrix():
+@pytest.mark.parametrize('n_v', [8, 9, 10])
+def test_get_adjacency_matrix(n_v):
     """ Test get_adjacency_matrix against NetworkX version """
-    n = 10
     for _ in range(1000):
-        graph = random_connected_graph(n)
+        graph = random_connected_graph(n_v)
         nodes = sorted(graph.nodes())
         test_adj_mat, key = get_adjacency_matrix(graph)
         nx_adj_mat = nx.to_numpy_array(graph, nodelist=nodes, dtype=int)
